b9d86288e3198b42ec275c581627f61de3f63c4e
e2e4 <sosensky@mail.ru>
2018-10-16 12:47:31 +0300

    footer

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+
+  var footerSponsorsSlider = new Swiper ('#footer-sponsors-slider', {
+
+    navigation: {
+      nextEl: '#footer-sponsors-slider-next',
+      prevEl: '#footer-sponsors-slider-prev',
+    },
+
+    spaceBetween: 20,
+    slidesPerView: 'auto'
+
+  });
+
+  var footerPartnersSlider = new Swiper ('#footer-partners-slider', {
+
+    navigation: {
+      nextEl: '#footer-partners-slider-next',
+      prevEl: '#footer-partners-slider-prev',
+    },
+
+    spaceBetween: 20,
+    slidesPerView: 'auto'
+
+  });
+
+
+});

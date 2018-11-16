3e283ddd305b4d7eea54e707386c2674edeafb63
e2e4 <sosensky@mail.ru>
2018-11-16 10:13:59 +0300

    page users db

---
+++ js/db_users.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  $('.i-check').iCheck({
+    checkboxClass: 'icheckbox_quacol',
+    radioClass: 'iradio_quacol',
+    //increaseArea: '20%' // optional
+  });
+
+});

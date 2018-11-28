5231140ee3e152c9fe918cfc416ddb509229cf54
e2e4 <sosensky@mail.ru>
2018-11-28 15:04:13 +0300

    users db col search

--- js/db_users.js
+++ js/db_users.js
@@ -6,4 +6,20 @@ $(document).ready(function () {
     //increaseArea: '20%' // optional
   });
 
+  var $dbTable = $('#users-db-app .db-table');
+
+  $dbTable.find('th input[type=text]').keyup(function () {
+
+    var that = this;
+    var colIndex = $(this).closest('th').index();
+
+    $dbTable.find('tbody tr').filter(function () {
+
+      $(this).toggle($(this).find('td').eq(colIndex).text().toLowerCase().indexOf($(that).val().toLowerCase()) > -1);
+
+    });
+
+  });
+
+
 });

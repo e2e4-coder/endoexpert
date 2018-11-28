c7e764bc938d443bc0ec06c9bb9551bf255aefde
e2e4 <sosensky@mail.ru>
2018-11-28 16:57:11 +0300

    move to local/assets

---
+++ local/assets/js/lk.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+
+  var lkSlider = new Swiper ('#lk-slider', {
+
+    pagination: {
+      el: '#lk-slider .swiper-pagination',
+      clickable: true
+    },
+    loop: true
+  });
+
+  $('.lk-comments-views-rating .rating select').each(function () {
+
+    $(this).barrating({
+      theme: 'small-white-stars',
+      initialRating: $(this).data('current-rating'),
+      readonly: true,
+      allowEmpty: true,
+      emptyValue: 0,
+    });
+
+  });
+
+
+  $('.add-to-favorite').click(function () {
+
+    var that = this;
+
+    $.post('/local/assets/json/get-one.json', {
+
+      action: 'add-to-favorite',
+      iblock_id : $(this).closest('.lk-news-module').data('iblock-id'),
+      element_id : $(this).closest('.lk-news-module').data('element-id'),
+
+    }, function (data) {
+
+      if (data === 1) {$(that).addClass('active');} else {$(that).removeClass('active');}
+
+    });
+
+  });
+
+  $('.add-to-learn').click(function () {
+
+    var that = this;
+
+    $.post('/local/assets/json/get-one.json', {
+
+      action: 'add-to-learn',
+      iblock_id : $(this).closest('.lk-news-module').data('iblock-id'),
+      element_id : $(this).closest('.lk-news-module').data('element-id'),
+
+    }, function (data) {
+
+      if (data === 1) {$(that).addClass('active');} else {$(that).removeClass('active');}
+
+    });
+
+  });
+
+
+
+});
---
+++ local/assets/js/lk_publication.js
@@ -0,0 +1,134 @@
+
+
+$(document).ready(function () {
+
+  $('.i-check').iCheck({
+    checkboxClass: 'icheckbox_quacol',
+    radioClass: 'iradio_quacol',
+    //increaseArea: '20%' // optional
+  });
+
+  $('.lk-comments-views-rating .rating select').each(function () {
+
+    $(this).barrating({
+      theme: 'small-white-stars',
+      initialRating: $(this).data('current-rating'),
+      readonly: true,
+      allowEmpty: true,
+      emptyValue: 0,
+    });
+
+  });
+
+  $('#publication-rating-select').change(function () {
+
+    $.post('/local/assets/json/post-rating.json', {
+
+      rating : $(this).val(),
+      id : $(this).data('publication-id'),
+
+    }, function () {
+
+
+    });
+
+  }).barrating({
+    theme: 'fa-stars',
+    initialRating: $('#publication-rating-select').data('current-rating'),
+    allowEmpty: true,
+    emptyValue: 0,
+  });
+
+
+
+  $('#publication-subscribe-topic').on('ifChanged', function (event) {
+
+    $.post('/local/assets/json/get-one.json', {
+      action : $(event.target).is(':checked') ? 'subscribe' : 'unsubscribe',
+      topic_id : $(event.target).data('topic-id')
+    }).done(function (data) {
+
+      if (!data) {$(event.target).iCheck('uncheck');}
+
+    }).fail(function(xhr, status, error) {
+      $(event.target).iCheck('uncheck');
+    });
+  });
+
+  $('#publication-subscribe-author').on('ifChanged', function (event) {
+
+    $.post('/local/assets/json/get-one.json', {
+      action : $(event.target).is(':checked') ? 'subscribe' : 'unsubscribe',
+      topic_id : $(event.target).data('author-id')
+    }).done(function (data) {
+
+      if (!data) {$(event.target).iCheck('uncheck');}
+
+    }).fail(function(xhr, status, error) {
+      $(event.target).iCheck('uncheck');
+    });
+  });
+
+
+  $('.endoexpert-poll').flip({
+    trigger: 'manual'
+  }).each(function () {
+
+    $(this).find('.poll-results').height($(this).find('.poll-question').height())
+
+  });
+
+  $('.flip-toggle').click(function (e) {
+
+    $('.endoexpert-poll').flip('toggle');
+
+    e.preventDefault();
+    return false;
+
+  });
+
+  $('#invite-friends-button').click(function () {
+
+    $.magnificPopup.open({
+      items: {
+        src: '#invite-friends-popup'
+      },
+      modal: true,
+      type: 'inline',
+      tClose: 'Закрыть (Esc)',
+      tLoading: 'Загрузка...',
+      callbacks: {
+        open: initInviteFiendsApp(function () {
+
+          $.ajax({
+            url: '/local/assets/json/get-one.json',
+            type: 'post',
+            data: {topic_id: inviteFriendsApp.topic_id, friends : inviteFriendsApp.selected_items},
+            cache: false,
+            success: function(){
+              $.magnificPopup.close();
+              $.magnificPopup.open({
+                items: {
+                  src: '#invite-friends-popup-success'
+                },
+                type: 'inline',
+                tClose: 'Закрыть (Esc)',
+                tLoading: 'Загрузка...',
+
+              });
+            },
+            error: function(){
+              alert('error!');
+            }
+          });
+
+        }),
+        close: destroyInviteFiendsApp
+      }
+    });
+
+  });
+
+
+
+});

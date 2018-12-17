dcd7112aa65e67b4cf5c2d53d70e67d1ccebc3b0
e2e4 <sosensky@mail.ru>
2018-12-17 11:24:04 +0300

    add publication fix

--- local/assets/js/lk_add_publication.js
+++ local/assets/js/lk_add_publication.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
     $('.i-check').iCheck({
       checkboxClass: 'icheckbox_quacol',
-      radioClass: 'icheckbox_quacol',
+      radioClass: 'iradio_quacol',
       //increaseArea: '20%' // optional
     });
 

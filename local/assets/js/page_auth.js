0a96eba94438242f46669fda08788b2168b067e1
e2e4 <sosensky@mail.ru>
2019-01-16 17:18:32 +0300

    page auth

---
+++ local/assets/js/page_auth.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  if ($('#page-auth').length) {
+
+    $('#page-auth .i-check-ee').iCheck({
+      checkboxClass: 'icheckbox_ee',
+      radioClass: 'iradio_ee'
+    });
+
+
+
+
+  }
+});

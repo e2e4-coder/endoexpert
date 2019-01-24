50f60716eedff6ab0058d1304a28ab73dddc9fbe
e2e4 <sosensky@mail.ru>
2019-01-24 10:08:41 +0300

    page auth adds

--- local/assets/js/page_auth.js
+++ local/assets/js/page_auth.js
@@ -8,6 +8,17 @@ $(document).ready(function () {
     });
 
 
+    $('#select-user-status-pics .status-item').click(function () {
+
+      $('#select-user-status-pics .status-item').removeClass('-selected');
+
+      $(this).addClass('-selected');
+
+      $('#select-user-status-pics input[type=hidden]').val($(this).data('value'))
+
+    });
+
+
 
 
   }

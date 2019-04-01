98602f06fe74208046eb6d05058201216b44e63d
e2e4 <sosensky@mail.ru>
2019-04-01 16:08:25 +0300

    page content

---
+++ local/assets/js/page_content.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+  var $typeSelect = $('.ee-search-block .type-select');
+
+  $typeSelect.find('> span').click(function () {
+
+    $typeSelect.find('ul').toggle();
+
+
+
+  });
+
+  $typeSelect.find('li').click(function () {
+
+    $typeSelect.find('input').val($(this).data('value'));
+    $typeSelect.find('> span').text($(this).text());
+    $typeSelect.find('ul').hide();
+  });
+
+
+
+  var sliders = [];
+
+  $('.content-section .swiper-container').each(function () {
+
+    var id = '#' + $(this).attr('id');
+
+    var autoplay = $(id).data('autoplay-delay');
+
+    autoplay = autoplay ? {delay: autoplay} : false;
+
+    sliders.push(new Swiper (id, {
+
+
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 14,
+      loopedSlides: 4,
+      loop: true,
+      navigation: {
+        nextEl: id + '-next',
+        prevEl: id + '-prev',
+      },
+
+      autoplay: autoplay,
+
+      breakpoints: {
+
+        1023: {
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          spaceBetween: 10
+        }
+      }
+    }));
+
+  });
+
+
+
+});

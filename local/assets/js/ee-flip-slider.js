499610fb6e3aac791352dd11243f8887ff070196
e2e4 <sosensky@mail.ru>
2019-06-28 12:26:30 +0300

    flip slider, add rate and comment

--- local/assets/js/ee-flip-slider.js
+++ local/assets/js/ee-flip-slider.js
@@ -21,6 +21,30 @@ $(document).ready(function () {
 
 
 
+  $('.ee-flip-slider .rating-section select').each(function () {
+
+    $(this).change(function () {
+
+      $.post($(this).data('api-url'), {
+
+        rating : $(this).val(),
+        id : $(this).data('element-id'),
+
+      }, function () {
+
+
+      });
+
+    }).barrating({
+      theme: 'fa-stars-gold2',
+      initialRating: $(this).data('current-rating'),
+      readonly: !!$(this).data('voted'),
+      allowEmpty: true,
+      emptyValue: 0,
+    });
+
+  });
+
 
 
 });

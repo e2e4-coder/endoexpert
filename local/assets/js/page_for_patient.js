6a9dae821edcd8b1e4d4ea55e9444dc54c1843b9
e2e4 <sosensky@mail.ru>
2019-12-13 21:13:59 +0300

    page for patient fix

--- local/assets/js/page_for_patient.js
+++ local/assets/js/page_for_patient.js
@@ -1,10 +1,37 @@
 $(document).ready(function () {
 
+  $('.main-slider').each(function () {
+
+    var $swiper = $(this).find('.swiper-container');
+
+    var delay  = $swiper.data('autoplay-delay') ? {delay: $swiper.data('autoplay-delay')} : false;
+
+    new Swiper ($swiper, {
+
+      slidesPerView: 1,
+      loop: true,
+      navigation: {
+        nextEl: $(this).find('.swiper-button-next'),
+        prevEl: $(this).find('.swiper-button-prev'),
+      },
+
+      autoplay: delay,
+
+    });
+
+
+
+
+  });
+
+
   $('.ee-search-block-2 .last-queries span').click(function () {
 
     $('.ee-search-block-2 .form-text').val($(this).text());
 
   });
+
+  $('.ee-sections-cards-list .section-card').flip({trigger: 'hover'});
 
 });
 

c78805ddac83d5892058429c108d4a8b611cf722
e2e4 <sosensky@mail.ru>
2019-12-20 10:36:06 +0300

    page for patient fix

--- local/assets/js/page_for_patient.js
+++ local/assets/js/page_for_patient.js
@@ -24,6 +24,41 @@ $(document).ready(function () {
 
   });
 
+  $('.drugs-slider').each(function () {
+
+    var $swiper = $(this).find('.swiper-container');
+
+    var delay  = $swiper.data('autoplay-delay') ? {delay: $swiper.data('autoplay-delay')} : false;
+
+    new Swiper ($swiper, {
+
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 14,
+
+      navigation: {
+        nextEl: $(this).find('.swiper-button-next'),
+        prevEl: $(this).find('.swiper-button-prev'),
+      },
+
+      autoplay: delay,
+
+      breakpoints: {
+
+        1023: {
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          spaceBetween: 10
+        }
+      }
+    });
+
+
+
+
+
+  });
+
 
   $('.ee-search-block-2 .last-queries span').click(function () {
 

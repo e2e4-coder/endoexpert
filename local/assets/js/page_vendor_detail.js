90475de29ff79add215a38f8d6d8d1845e04548c
e2e4 <sosensky@mail.ru>
2019-12-27 16:26:13 +0300

    page vendor detail

---
+++ local/assets/js/page_vendor_detail.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  $(document).on('click', 'a[href^="#"]', function(e) {
+
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $('a[name=' + $.attr(this, 'href').replace('#', '') + ']').offset().top -100
+    }, 500);
+  });
+
+});
+

8ef0402ddd0c9d6c8d74066314beab4261e4dbde
e2e4 <sosensky@mail.ru>
2020-02-13 14:36:58 +0300

    ee-copy-link-html

--- local/assets/js/common.js
+++ local/assets/js/common.js
@@ -571,6 +571,11 @@ $(document).ready(function () {
   });
 
 
+  $('.ee-copy-link-html').click(function () {
+
+    copyFormattedToClipboard('<a href="'+window.location.href+'">'+$(this).parent().text().trim()+'</a>');
+
+  });
 
 
 });
@@ -598,6 +603,53 @@ function copyToClipboard(str) {
     }
 
 
+}
+
+function copyFormattedToClipboard (html) {
+  // Create container for the HTML
+  // [1]
+  var container = document.createElement('div')
+  container.innerHTML = html
+
+  // Hide element
+  // [2]
+  container.style.position = 'fixed'
+  container.style.pointerEvents = 'none'
+  container.style.opacity = 0
+
+  // Detect all style sheets of the page
+  var activeSheets = Array.prototype.slice.call(document.styleSheets)
+      .filter(function (sheet) {
+        return !sheet.disabled
+      })
+
+  // Mount the container to the DOM to make `contentWindow` available
+  // [3]
+  document.body.appendChild(container)
+
+  // Copy to clipboard
+  // [4]
+  window.getSelection().removeAllRanges()
+
+  var range = document.createRange()
+  range.selectNode(container)
+  window.getSelection().addRange(range)
+
+  // [5.1]
+  document.execCommand('copy')
+
+  // [5.2]
+  for (var i = 0; i < activeSheets.length; i++) activeSheets[i].disabled = true
+
+  // [5.3]
+  document.execCommand('copy')
+
+  // [5.4]
+  for (var i = 0; i < activeSheets.length; i++) activeSheets[i].disabled = false
+
+  // Remove the container
+  // [6]
+  document.body.removeChild(container)
 }
 
 window.showPopup = function (options) {
